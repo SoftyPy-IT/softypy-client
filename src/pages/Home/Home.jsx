74a9c09b8def094f74e320b5acc1180a1aca14ac
Ibrahim-Sikder <ibrahimsikder5033@gmail.com>
2024-03-10 10:03:53 +0600

    erp-package

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,12 +8,17 @@ import TimeLine from "./TimeLine/TimeLine";
 import CountDown from "./CountDown/CountDown";
 import HeroSection from "../../components/Banner/HeroSection/HeroSection";
 import Subscribe from "../../components/Subscribe/Subscribe";
+import Accordion from "./Practice";
+import Accordion3 from "./Practice2";
 
 const Home = () => {
   return (
     <div className="home">
       <HeroSection />
+      <Accordion />
+      <Accordion3 />
       <WhoWeAre />
+
       <Services />
       <SwiperTimeLine />
       <TimeLine />
@@ -24,7 +29,6 @@ const Home = () => {
       <Subscribe />
     </div>
   );
-  
 };
 
 export default Home;
