a16a5e702d96c5eaa0a0f7ef5bb2e4ade0750f61
Ibrahim-Sikder <ibrahimsikder5033@gmail.com>
2024-03-12 13:57:39 +0600

    services-page-responsive

--- src/pages/Services/TopServices/TopServices.jsx
+++ src/pages/Services/TopServices/TopServices.jsx
@@ -42,8 +42,8 @@ export default Arrow = () => {
             <h2 className="text-3xl md:text-5xl">Our Top Services</h2>
           </div>
         </div>
-
-        <div className="grid grid-cols-2 mt-20">
+ mb-8
+        <div className="grid grid-cols-1 lg:grid-cols-2 mt-20 mb-8">
           <div className="topServicesLeft">
             <lottie-player
               id="firstLottie"
@@ -55,26 +55,38 @@ export default Arrow = () => {
               className="animation"
             ></lottie-player>
           </div>
-
           <div className="topServiceRight">
-            <h2 className="text-4xl font-bold">Web Design & Development </h2>
-            <p className="my-3 leading-7">
-            Unlock the full potential of your online presence with our bespoke website design and development services. Our team of skilled designers and developers work collaboratively to create visually stunning, user-friendly websites that not only captivate your audience but also provide seamless navigation. From responsive design to e-commerce solutions, we ensure that your website is a true reflection of your brand and engages visitors effectively.
+          <h2 className="text-4xl font-bold">Web Design & Development </h2>
+          <p className="my-3 leading-7">
+            Unlock the full potential of your online presence with our bespoke
+            website design and development services. Our team of skilled
+            designers and developers work collaboratively to create visually
+            stunning, user-friendly websites that not only captivate your
+            audience but also provide seamless navigation. From responsive
+            design to e-commerce solutions, we ensure that your website is a
+            true reflection of your brand and engages visitors effectively.
+          </p>
 
-            </p>
-
-            <div className="flex items-center cursor-pointer">
-              <span>Learn More </span>
-              <FaArrowRight className="ml-2 text-[#F81600] " />
-            </div>
+          <div className="flex items-center cursor-pointer">
+            <span>Learn More </span>
+            <FaArrowRight className="ml-2 text-[#F81600] " />
           </div>
+        </div>
+        </div>
+       
 
-
-          <div className="topServiceRight">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 mb-8">
+          <div className="topServiceRight order-3 lg:order-1">
             <h2 className="text-4xl font-bold">App Development​​</h2>
             <p className="my-3 leading-7">
-            Stay ahead of the curve with our innovative app development services. Whether you're looking to create a mobile application for iOS, Android, or cross-platform, we've got you covered. Our experienced team leverages the latest technologies to build intuitive and feature-rich apps that cater to your unique business requirements. From concept to deployment, we prioritize user experience and functionality to ensure your app stands out in the competitive digital landscape.
-
+              Stay ahead of the curve with our innovative app development
+              services. Whether you're looking to create a mobile application
+              for iOS, Android, or cross-platform, we've got you covered. Our
+              experienced team leverages the latest technologies to build
+              intuitive and feature-rich apps that cater to your unique business
+              requirements. From concept to deployment, we prioritize user
+              experience and functionality to ensure your app stands out in the
+              competitive digital landscape.
             </p>
 
             <Link to="/services">
@@ -84,7 +96,7 @@ export default Arrow = () => {
               </div>
             </Link>
           </div>
-          <div className="topServicesLeft">
+          <div className="topServicesLeft order-2">
             <lottie-player
               id="firstLottie"
               ref={ref}
@@ -95,10 +107,9 @@ export default Arrow = () => {
               className="animation"
             ></lottie-player>
           </div>
+        </div>
 
-
-
-
+        <div className="grid grid-cols-1 gap-5 lg:grid-cols-2 mb-8">
           <div className="topServicesLeft">
             <lottie-player
               id="firstLottie"
@@ -113,8 +124,14 @@ export default Arrow = () => {
           <div className="topServiceRight">
             <h2 className="text-4xl font-bold">ERP Software Solution</h2>
             <p className="my-3 leading-7">
-            Optimize your business processes with our robust ERP (Enterprise Resource Planning) software solutions. We understand the importance of efficiency and integration in today's fast-paced business environment. Our ERP solutions are tailored to streamline your operations, enhance collaboration, and provide real-time insights into your business data. Experience increased productivity and seamless workflow management with our customized ERP systems.
-
+              Optimize your business processes with our robust ERP (Enterprise
+              Resource Planning) software solutions. We understand the
+              importance of efficiency and integration in today's fast-paced
+              business environment. Our ERP solutions are tailored to streamline
+              your operations, enhance collaboration, and provide real-time
+              insights into your business data. Experience increased
+              productivity and seamless workflow management with our customized
+              ERP systems.
             </p>
             <Link to="/services">
               <div className="flex items-center cursor-pointer">
@@ -122,35 +139,45 @@ export default Arrow = () => {
                 <FaArrowRight className="ml-2 text-[#F81600] " />
               </div>
             </Link>
-          </div>
-
-          <div className="topServiceRight mt-10">
-            <h2 className="text-4xl font-bold">Digital Marketing​</h2>
-            <p className="my-3 leading-7">
-            Amplify your online presence and reach your target audience with our comprehensive digital marketing services. Our team of experts specializes in creating data-driven strategies across various channels, including social media, search engine optimization (SEO), content marketing, and more. Whether you're looking to boost brand awareness, drive traffic, or increase conversions, our digital marketing solutions are designed to deliver tangible and measurable results.
-
-            </p>
-
-            <Link to="/services">
-              <div className="flex items-center cursor-pointer">
-                <span>Learn More </span>
-                <FaArrowRight className="ml-2 text-[#F81600] " />
-              </div>
-            </Link>
-          </div>
-
-          <div className="topServicesLeft">
-            <lottie-player
-              id="firstLottie"
-              ref={ref}
-              autoplay
-              loop
-              mode="normal"
-              src="/content.json"
-              className="animation"
-            ></lottie-player>
           </div>
         </div>
+
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 mb-8">
+        <div className="topServiceRight mt-10 order-3 lg:order-1">
+        <h2 className="text-4xl font-bold">Digital Marketing​</h2>
+        <p className="my-3 leading-7">
+          Amplify your online presence and reach your target audience with our
+          comprehensive digital marketing services. Our team of experts
+          specializes in creating data-driven strategies across various
+          channels, including social media, search engine optimization (SEO),
+          content marketing, and more. Whether you're looking to boost brand
+          awareness, drive traffic, or increase conversions, our digital
+          marketing solutions are designed to deliver tangible and measurable
+          results.
+        </p>
+
+        <Link to="/services">
+          <div className="flex items-center cursor-pointer">
+            <span>Learn More </span>
+            <FaArrowRight className="ml-2 text-[#F81600] " />
+          </div>
+        </Link>
+      </div>
+
+      <div className="topServicesLeft order-2">
+        <lottie-player
+          id="firstLottie"
+          ref={ref}
+          autoplay
+          loop
+          mode="normal"
+          src="/content.json"
+          className="animation"
+        ></lottie-player>
+      </div>
+        </div>
+
+       
       </main>
     </Container>
   );
