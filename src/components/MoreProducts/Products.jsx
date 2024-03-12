3014ec89391636a1b22c7d9912f4458d284ff21e
Ibrahim-Sikder <ibrahimsikder5033@gmail.com>
2024-03-12 14:36:06 +0600

    single-services-page-responsive

--- src/components/MoreProducts/Products.jsx
+++ src/components/MoreProducts/Products.jsx
@@ -5,18 +5,18 @@ const Products = () => {
   const cardData = [
     {
       id: 1,
-      name: "Web & APP Development",
+      name: "APPS Development",
       desc:"SoftyPy specializes in crafting dynamic websites and innovative mobile apps, delivering a seamless user experience and digital solutions tailored to your needs."
     },
     {
       id: 2,
-      name: "ERP Solutions",
+      name: "Web Development",
       desc: "ERP Solutions optimize business operations through integrated software, fostering efficiency and informed decision-making for sustained growth."
     },
     
     {
       id: 3,
-      name: "UI/UX Design ​",
+      name: "ERP Solutions ​",
       desc: "At SoftyPy, we craft exceptional UI/UX designs that harmonize aesthetics and functionality, ensuring delightful user experiences for your digital products."
     },
     {
@@ -24,18 +24,14 @@ const Products = () => {
       name: "Digital Marketing",
       desc:"SoftyPy drives business growth with data-driven digital marketing strategies, elevating your online presence and engaging target audiences for lasting results."
     },
-    // {
-    //   id: 5,
-    //   name: "SEO ",
-    //   desc: 'SoftyPy SEO services can build your high rankings and achieving your organic traffic & improve search engine result.'
-    // },
+
     
   ];
   return (
-    <div className="my-24">
-      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 place-items-center gap-6">
+    <div className="mt-0 lg:mt-20">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 place-items-center place-content-center gap-5 ">
         {cardData.map((data) => (
-          <div key={data.id} className="productCard">
+          <div key={data.id} className="productCard mt-8">
             <div className="moreServiceIconWrap">
            <FaLaptopCode className="" size={70} />
             </div>
